10cfcd4427a1de4f262434911f5fadf6d76cb708
M. Mahdani Rahmatullah <50037482+mahdanidn@users.noreply.github.com>
2023-02-06 13:59:47 +0700

    setup tailwind

---
+++ src/app/fonts.tsx
@@ -0,0 +1,17 @@
+import { DM_Sans, Inter, Roboto_Mono } from '@next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'optional'
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'optional',
+});
+
+export const dm_sans = DM_Sans({
+  weight: "400",
+  style: "normal",
+  display: 'optional',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
-import './globals.css'
+import { Inter, Roboto_Mono } from '@next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'optional',
+});
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
+  display: 'optional',
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
+      <body>
+        <h1>My App</h1>
+        <div>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
